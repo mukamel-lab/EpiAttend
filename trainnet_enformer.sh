3c5ac34381b1848d749b93007b7efc52c92a1463
Eran <emukamel@ucsd.edu>
2022-10-03 10:39:19 -0700

    local trainnet

--- trainnet_enformer.sh
+++ trainnet_enformer.sh
@@ -6,7 +6,7 @@ source $HOME/.bashrc
 module load cuda/11.2 # Note: 11.2 is not working
 export TF_XLA_FLAGS=--tf_xla_enable_xla_devices
 
-./mypython /tuba/datasets/enformer/scripts/trainnet_enformer.py "$@" 
+./mypython ./trainnet_enformer.py "$@" 
 
 exit
 
